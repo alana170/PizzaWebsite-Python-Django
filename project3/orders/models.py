bad6a044bf80c952de7bd7183f1134677aee3d5f
alana170 <52054877+alana170@users.noreply.github.com>
2020-07-28 16:42:30 -0400

    login/signup setup, django admin created two models

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model): 
+    name= models.CharField(max_length=90)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name= models.CharField(max_length=90)
+    category= models.ForeignKey(ProductCategory,on_delete=models.CASCADE,related_name="category")
+    SPrice= models.FloatField()
+    LPrice= models.FloatField()
+    toppings= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}  Small: {self.SPrice} Large: {self.LPrice}"
--- project3/orders/views.py
+++ project3/orders/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from . import views 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if not request.user.is_authenticated:
+        return render(request,'login.html')
+    return render(request, 'home.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
