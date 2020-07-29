92ea9b311279fd5eb36d2ad2e7715a3130417c94
alana170 <52054877+alana170@users.noreply.github.com>
2020-07-29 19:05:18 -0400

    navbar setup

--- project3/orders/urls.py
+++ project3/orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("signup.html", views.signup, name="signup")
+    path("signup", views.signup, name="signup"),
+    path("login", views.signin, name="login")
 ]
--- project3/orders/views.py
+++ project3/orders/views.py
@@ -1,14 +1,42 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from . import views 
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return render(request,'login.html')
-    return render(request, 'home.html')
+
+    context = {
+        "user": request.user
+    }
+    
+    return render(request, 'home.html', context)
 
 def signup(request):
+    if request.method =='POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        name = request.POST["name"]
+        email = request.POST["email"]
+        user = User.objects.create_user(username, email, password)
+        user.first_name = name
+        user.save()
+        return render(request, 'login.html',{"message": "You have successfully created an account. Thank you for registration. "})  
     return render(request, 'signup.html')
 
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "home.html")
+        else:
+            return render(request, "login.html", {"message": "Invalid credentials."})
+
+    return render(request, 'login.html')
